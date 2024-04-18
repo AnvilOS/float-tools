7725cc7b63624d310a2ac2949aa62519697d9622
Gerard Green <gerryg@anvilos.com>
2024-04-18 20:01:37 +1000

    First commit with submodule

---
+++ src/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+#include "xint.h"
+
+int main(int argc, const char * argv[])
+{
+    xint_t X;
+    xint_init(X, 20);
+    
+    // insert code here...
+    printf("Hello, World!\n");
+    
+    return 0;
+}
