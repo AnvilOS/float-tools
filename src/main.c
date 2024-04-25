92e69081b8907d7b64ba4f405d8349bffafc4d57
Gerard Green <gerryg@anvilos.com>
2024-04-25 10:17:52 +1000

    Compare anvil_dtoa and dragon4 with dtoa

--- src/main.c
+++ src/main.c
@@ -2,20 +2,33 @@
 #include "dragon4.h"
 #include "xint.h"
 #include "ieee754.h"
+#include "anvil_dtoa.h"
 
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fenv.h>
+#include <math.h>
+#include <time.h>
 
-char *dtoa(double dd, int mode, int ndigits, int *decpt, int *sign, char **rve);
+#include "gdtoa.h"
 
 int cmp_dragon4_w_dtoa(double d);
+int cmp_anvil_w_dtoa(double d);
 int run_all_test_cases(int (*f)(double));
 
 int main(int argc, const char * argv[])
 {
-    run_all_test_cases(cmp_dragon4_w_dtoa);
+    static double time_consumed = 0;
+    clock_t start, end;
+    start = clock();
+    
+    run_all_test_cases(cmp_anvil_w_dtoa);
+    
+    end = clock();
+    time_consumed += (double)(end - start) / CLOCKS_PER_SEC;
+    printf("Time=%f\n", time_consumed);
     return 0;
 }
 
@@ -33,16 +46,36 @@ int cmp_dragon4_w_dtoa(double d)
     {
         printf("Mismatch between dtoa mode 0 and Dragon4\n");
         printf("%.30e ", d);
-        printf("0x%016llx ", double_to_hex(d));
-        printf("%s %d ", pstr1, decpt1);
+        printf("0x%016llx\n", double_to_hex(d));
+        printf("%s %d\n", pstr1, decpt1);
         printf("%s %d\n", pstr2, decpt2);
         printf("NG\n\n");
         return -1;
     }
-//    printf("0x%016llx\n", double_to_hex(d));
-//    printf("%s %d\n", pstr1, decpt1);
-//    printf("%s %d\n", pstr2, decpt2);
-//    printf("OK\n");
+    return 0;
+}
+
+int cmp_anvil_w_dtoa(double d)
+{
+    uint64_t f;
+    int e;
+    int sign;
+    split_double(d, &sign, &f, &e);
+    int decpt1;
+    char *pstr1 = anvil_dtoa(d, 0, 0, &decpt1, &sign, NULL);
+    int decpt2;
+    char *pstr2 = dtoa(d, 0, 0, &decpt2, &sign, NULL);
+    if ((decpt1 != decpt2) || strcmp(pstr1, pstr2))
+    {
+        printf("Mismatch between dtoa mode 0 and Dragon4\n");
+        printf("%.30e ", d);
+        printf("0x%016llx\n", double_to_hex(d));
+        printf("%s %d\n", pstr1, decpt1);
+        printf("%s %d\n", pstr2, decpt2);
+        printf("NG\n\n");
+        return -1;
+    }
+    free(pstr1);
     return 0;
 }
 
@@ -102,11 +135,6 @@ int run_all_test_cases(int (*f)(double))
             {
                 uint32_t x4;
                 union { uint32_t bits; float fp; } x8;
-                char num_str[1000];
-                if (fgets(line, 999, fin) == NULL)
-                {
-                    break;
-                }
                 res = sscanf(line, "%x %x %llx %s", &x4, &x8.bits, &nn, num_str);
                 if (res == -1)
                 {
